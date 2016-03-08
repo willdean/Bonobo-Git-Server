59bb52644d010fbd6b1ed83ed3aafd08507b245f
Will Dean <will.dean@indcomp.co.uk>
2016-03-08 16:35:36 +0000

    Move repo uniqueness test into RepoRepo, simplify validation

--- Bonobo.Git.Server/Controllers/ValidationController.cs
+++ Bonobo.Git.Server/Controllers/ValidationController.cs
@@ -1,13 +1,7 @@
-﻿using Bonobo.Git.Server.Attributes;
-using Bonobo.Git.Server.Data;
-using Bonobo.Git.Server.Models;
+﻿using Bonobo.Git.Server.Data;
 using Bonobo.Git.Server.Security;
 using Microsoft.Practices.Unity;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.UI;
 
@@ -25,41 +19,21 @@ namespace Bonobo.Git.Server.Controllers
         [Dependency]
         public ITeamRepository TeamRepo { get; set; }
 
-        public ActionResult UniqueNameRepo(string name, string guid)
+        public ActionResult UniqueNameRepo(string name, Guid? id)
         {
-            //Guid id = guid.HasValue ? guid.Value : Guid.Empty;
-            string sid = Request.QueryString["Id"];
-            Guid id = sid == "undefined" ? Guid.Empty : Guid.Parse(sid);
-            var existing_repo = new RepositoryDetailModel();
-            try
-            {
-                existing_repo = RepositoryController.ConvertRepositoryModel(RepoRepo.GetRepository(id), User);
-            }catch(Exception)
-            {
-            }
-            var validationContext = new ValidationContext(existing_repo);
-            // This will do two repository lookups at least but keeps the
-            // logic behind the validation the same.
-            UniqueRepoNameAttribute uniqRepoName = new UniqueRepoNameAttribute();
-            var result = uniqRepoName.GetValidationResult(name, validationContext);
-            return Json(result == System.ComponentModel.DataAnnotations.ValidationResult.Success, JsonRequestBehavior.AllowGet);
+            bool isUnique = RepoRepo.NameIsUnique(name, id ?? Guid.Empty);
+            return Json(isUnique, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult UniqueNameUser(string Username, Guid? guid)
+        public ActionResult UniqueNameUser(string Username, Guid? id)
         {
-            //Guid id = guid.HasValue ? guid.Value : Guid.Empty;
-            string sid = Request.QueryString["Id"];
-            Guid id = sid == "undefined" ? Guid.Empty : Guid.Parse(sid);
             var possibly_existent_user = MembershipService.GetUserModel(Username);
             bool exists = (possibly_existent_user != null) && (id != possibly_existent_user.Id);
             return Json(!exists, JsonRequestBehavior.AllowGet);
         }
 
-        public ActionResult UniqueNameTeam(string name, Guid? guid)
+        public ActionResult UniqueNameTeam(string name, Guid? id)
         {
-            //Guid id = guid.HasValue ? guid.Value : Guid.Empty;
-            string sid = Request.QueryString["Id"];
-            Guid id = sid == "undefined" ? Guid.Empty : Guid.Parse(sid);
             var possibly_existing_team = TeamRepo.GetTeam(name);
             bool exists = (possibly_existing_team != null) && (id != possibly_existing_team.Id);
             // false when repo exists!
